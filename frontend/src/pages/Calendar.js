5e60d24ec8b834d9d2a1a13a35be9a7a26c8837a
willty412 <itssinq@gmail.com>
2023-04-24 02:11:42 -0400

    Added modal on click

--- frontend/src/pages/Calendar.js
+++ frontend/src/pages/Calendar.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Modal from 'react-modal';
 
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
@@ -6,6 +7,9 @@ const Calendar = () => {
     const [date] = useState(new Date());
     const [month, setMonth] = useState(date.getMonth());
     const [year, setYear] = useState(date.getFullYear());
+    const [selectedDate, setSelectedDate] = useState(null);
+    const [eventsDetails, setEventsDetails] = useState("");
+    const [modalIsOpen, setModalIsOpen] = useState(false);
 
     function prevMonth() {
         setMonth(month <= 0 ? 11 : month -1);
@@ -17,9 +21,26 @@ const Calendar = () => {
         setMonth(month >= 11 ? 0 : month + 1);
         setYear(month >= 11 ? year + 1 : year);
       }
+
+      function addEvent(date, eventDetails){
+        setEventsDetails(...eventsDetails, {date, eventDetails});
+      }
+
+      function handleDateClick(date){
+        setSelectedDate(date);
+        setModalIsOpen(true);
+      }
+
+      // function handleEventSubmit(e){
+      //   e.preventDefault();
+      //   addEvent(selectedDate, eventsDetails);
+      //   setSelectedDate(null);
+      //   setEventsDetails("");
+      //   setModalIsOpen(false);
+      // }
     
     function buildCalendar(){
-        let rows = [];
+    let rows = [];
     let cells = [];
     let firstDay = new Date(year, month, 1);
     let startDay = firstDay.getDay();
@@ -31,7 +52,12 @@ const Calendar = () => {
           cells.push(<td key={j}></td>);
         }else if(daysCounter < numDays){
           daysCounter++;
-          cells.push(<td key={j}>{daysCounter}</td>);
+          const date = new Date(year, month, daysCounter).toDateString();
+          cells.push(
+            <td key={j} onClick = {() => handleDateClick(date)}>
+            {daysCounter}
+          </td>
+          );
         }
         if(j === 6){
           rows.push(<tr key={i}>{cells}</tr>);
@@ -65,6 +91,7 @@ const Calendar = () => {
         </thead>
         <tbody id="calendar-body">{buildCalendar()}</tbody>
       </table>
+      <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}/>
      </div>
     )
 }
